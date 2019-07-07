3b16e570e0a5e6034a61490b65e4ddcc4c67b4a5
Jean-Marc Pussacq <jean-marc.pussacq@asso-cocktail.fr>
2019-07-07 21:18:51 +0200

    Gestion du combat par équipe + design + nouveau favicon

--- kata-pokemon-front-end/src/app/combat/combat.component.ts
+++ kata-pokemon-front-end/src/app/combat/combat.component.ts
@@ -13,6 +13,7 @@ export class CombatComponent implements OnInit {
 
   combatLance: boolean;
   combat: Combat;
+  pokemonAyantRecuAttaque: Pokemon;
 
   constructor(private pokemonService: PokemonService) { }
 
@@ -24,12 +25,14 @@ export class CombatComponent implements OnInit {
     this.pokemonService.initialiserCombat().subscribe(reponse => {
       this.combat = reponse;
       this.combatLance = true;
+      this.pokemonAyantRecuAttaque = null;
     });
   }
 
   lancerAttaqueSurPokemon(pokemonAttaquant: Pokemon, attaque: Attaque, pokemonRecevantAttaque: Pokemon) {
     this.pokemonService.lancerAttaque(pokemonAttaquant, attaque, pokemonRecevantAttaque).subscribe(reponse => {
       this.combat = reponse;
+      this.pokemonAyantRecuAttaque = pokemonRecevantAttaque;
     });
   }
 
@@ -47,4 +50,58 @@ export class CombatComponent implements OnInit {
     return 'warn';
   }
 
+  classForPokemon(pokemon: Pokemon) {
+    if (pokemon && this.pokemonAyantRecuAttaque) {
+      if (pokemon.id === this.pokemonAyantRecuAttaque.id) {
+        if (pokemon.vieActuelle <= 0) {
+          return 'ko';
+        }
+        return 'blesse';
+      } else if (pokemon.vieActuelle <= 0) {
+        return 'disparu';
+      }
+    }
+  }
+
+  nePeutPasLancerAttaque(pokemon: Pokemon): boolean {
+    if (pokemon.vieActuelle <= 0) {
+      return true;
+    }
+    if (!this.pokemonAyantRecuAttaque) {
+      return false;
+    }
+    if (this.combat.equipePokemon1 && this.combat.equipePokemon2) {
+        const pok1: Pokemon[] = this.combat.equipePokemon1.filter(
+            pokemonDeLequipe => pokemonDeLequipe.id === pokemon.id || pokemonDeLequipe.id === this.pokemonAyantRecuAttaque.id
+        );
+        const pok2: Pokemon[] = this.combat.equipePokemon2.filter(
+            pokemonDeLequipe => pokemonDeLequipe.id === pokemon.id || pokemonDeLequipe.id === this.pokemonAyantRecuAttaque.id
+        );
+        if (pok1.length === 2 || pok2.length === 2) {
+            return false;
+        }
+    }
+    if (this.pokemonAyantRecuAttaque.id === pokemon.id) {
+      return false;
+    }
+    return true;
+  }
+
+  pokemonsPouvantEtreAttaques(pokemons: Pokemon[]) {
+    const pokemonsPouvantEtreAttaques: Pokemon[] = [];
+    for (let i = 0; i < pokemons.length; i++) {
+      if (pokemons[i].vieActuelle > 0) {
+        pokemonsPouvantEtreAttaques.push(pokemons[i]);
+      }
+    }
+    return pokemonsPouvantEtreAttaques;
+  }
+
+  libelleBoutonAttaque(attaque: Attaque, pokemonRecevantAttaque: Pokemon, pokemonsAdverses: Pokemon[]): string {
+      const nbPokemonsPouvantEtreAttaques = this.pokemonsPouvantEtreAttaques(pokemonsAdverses).length;
+      if (nbPokemonsPouvantEtreAttaques > 1) {
+        return attaque.nom + ' (' + attaque.degats + ') -> ' + pokemonRecevantAttaque.nom;
+      }
+      return attaque.nom + ' (' + attaque.degats + ')';
+  }
 }
--- kata-pokemon-front-end/src/app/service/combat.ts
+++ kata-pokemon-front-end/src/app/service/combat.ts
@@ -3,4 +3,7 @@ import {Pokemon} from './pokemon';
 export class Combat {
   pokemon1: Pokemon;
   pokemon2: Pokemon;
+
+  equipePokemon1: Pokemon[];
+  equipePokemon2: Pokemon[];
 }
